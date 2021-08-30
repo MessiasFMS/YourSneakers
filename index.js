963504afc7b45f54f5aae73f2e4d029ac3071c0f
MessiasFMS <felipe.messias.silva@outlook.com>
2021-08-30 10:24:21 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(express.static('public'));
+app.use(express.urlencoded({extended: true}))
+
+app.get("/", function(req, res){
+    res.render('index.ejs');
+});
+
+app.listen(3000, () => {
+    console.log(`YourSneakers -> Online pape`);
+});
