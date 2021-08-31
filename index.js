37884f3a15463359e9a6bc6a933a26ec837a4feb
MessiasFMS <felipe.messias.silva@outlook.com>
2021-08-31 08:38:47 -0300

    Atualização

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(express.urlencoded({extended: true}))
 
-app.get("/", function(req, res){
+app.get("your-sneakerss.herokuapp.com/", function(req, res){
     res.render('index.ejs');
 });
 
