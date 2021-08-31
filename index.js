fd25bc959c0beeabd8fd891dd1fe9f63179a235f
MessiasFMS <felipe.messias.silva@outlook.com>
2021-08-31 08:42:11 -0300

    Update commit

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(express.urlencoded({extended: true}))
 
-app.get("your-sneakerss.herokuapp.com/", function(req, res){
+app.get("/", function(req, res){
     res.render('index.ejs');
 });
 
